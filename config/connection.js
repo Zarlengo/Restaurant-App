0c7f97925cabe8f7c1ce209a41c200b5fd8d6c78
Zarlengo <christopher@zarlengo.net>
2020-11-09 13:53:56 -0800

    Heroku test 2

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { Client } = require('pg');
-console.log(process.env.NODE_ENV);
 const connectionObject = require('./config.json')[process.env.NODE_ENV || "development"];
 
+console.log(connectionObject);
 const client = new Client(connectionObject);
 module.exports = client;
