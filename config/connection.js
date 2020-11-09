1cb6636a23f939d8db7a9e10a43a4e59333d93ec
Zarlengo <christopher@zarlengo.net>
2020-11-09 13:52:15 -0800

    Heroku fix

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,5 @@
 const { Client } = require('pg');
+console.log(process.env.NODE_ENV);
 const connectionObject = require('./config.json')[process.env.NODE_ENV || "development"];
 
 const client = new Client(connectionObject);
