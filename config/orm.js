15d5e27e54f2685827d4b3559c77f2f31bbac0fa
Zarlengo <christopher@zarlengo.net>
2020-11-09 13:23:15 -0800

    Changed to PostgreSQL for heroku

--- config/orm.js
+++ config/orm.js
@@ -1,38 +1,38 @@
-const connection = require("./connection.js");
-
-module.exports = {
-  selectAll: (tableInput, cb) => {
-    const queryString = "SELECT * FROM ??";
-    connection.query(queryString, [tableInput], (err, result) => {
-      if (err) throw err;
-      cb(result);
-    });
-  },
-
-  insertOne: (table, whereToInsert, whatToInsert, cb) => {
-    const queryString = "INSERT INTO ?? (??) VALUES (?)";
-    console.log(queryString);
-    connection.query(queryString, [table, whereToInsert, whatToInsert], (err, result) => {
-      if (err) throw err;
-      cb(result);
-    });
-  },
-
-  updateOne: (table, setObject, whereObj, cb) => {
-    setColumn = Object.keys(setObject)[0];
-    setValue = setObject[setColumn];
-
-    whereColumn = Object.keys(whereObj)[0];
-    whereValue = whereObj[whereColumn];
+module.exports = (connection) => {
+  return {
+    selectAll: (tableInput, cb) => {
+      const queryString = `SELECT * FROM ${ tableInput }`;
     
-    const queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
-    connection.query(
-      queryString,
-      [table, setColumn, setValue, whereColumn, whereValue],
-      function(err, result) {
+      connection.query(queryString, (err, result) => {
         if (err) throw err;
         cb(result);
-      }
-    );
+      });
+      
+    },
+
+    insertOne: (table, whereToInsert, whatToInsert, cb) => {
+      const queryString = `INSERT INTO ${ table } (${ whereToInsert }) VALUES (${ whatToInsert })`;
+      console.log(queryString);
+      connection.query(queryString, (err, result) => {
+        if (err) throw err;
+        cb(result);
+      });
+    },
+
+    updateOne: (table, setObject, whereObj, cb) => {
+      setColumn = Object.keys(setObject)[0];
+      setValue = setObject[setColumn];
+
+      whereColumn = Object.keys(whereObj)[0];
+      whereValue = whereObj[whereColumn];
+      
+      const queryString = `UPDATE ${ table } SET ${ setColumn } = ${ setValue } WHERE ${ whereColumn } = ${ whereValue }`;
+
+      connection.query(queryString, (err, result) => {
+          if (err) throw err;
+          cb(result);
+        }
+      );
+    }
   }
 };
--- controllers/burger_controller.js
+++ controllers/burger_controller.js
@@ -1,41 +1,43 @@
-const Express = require("express");
-const router = Express.Router();
-// Import the database settings
-const burgers = require("../models/burger.js");
+module.exports = (client) => {
+    const Express = require("express");
+    const router = Express.Router();
+    // Import the database settings
+    const burgers = require("../models/burger.js")(client);
 
-router.get("/", (req, res) => {
-    burgers.all(data => {
-        res.render("index", {burgers: data});
-    });
-});
-
-router.get("/api/burger", (req, res) => {
-    burgers.all(data => {
-        res.json({burgers: data});
-    });
-});
-
-router.post("/api/burger", (req, res) => {
-    burgers.create(
-        ["burger_name","devoured"],
-        [req.body.burger_name, req.body.devoured],
-        result => {
-            res.json({id: result.insertId})
+    router.get("/", (req, res) => {
+        burgers.all(data => {
+            res.render("index", {burgers: data.rows});
         });
-});
+    });
 
-router.put("/api/burger/:id", (req, res) => {
-    burgers.update({
-        devoured: req.body.devoured},
-        {id: req.params.id}, 
-        result => {
-            if (result.changedRoes == 0) {
-                return res.status(404).end();
-            } else {
-                res.status(200).end();
+    router.get("/api/burger", (req, res) => {
+        burgers.all(data => {
+            res.json({burgers: data.rows});
+        });
+    });
+
+    router.post("/api/burger", (req, res) => {
+        burgers.create(
+            ["burger_name","devoured"],
+            [`'${ req.body.burger_name }'`, req.body.devoured],
+            result => {
+                res.json({id: result.insertId})
+            });
+    });
+
+    router.put("/api/burger/:id", (req, res) => {
+        burgers.update({
+            devoured: req.body.devoured},
+            {id: req.params.id}, 
+            result => {
+                if (result.changedRoes == 0) {
+                    return res.status(404).end();
+                } else {
+                    res.status(200).end();
+                }
             }
-        }
-    );
-});
+        );
+    });
 
-module.exports = router;
+    return router;
+}
