c5a653b7a1618a9d2b79277d7e167c4613d8a810
Zarlengo <christopher@zarlengo.net>
2020-11-10 09:07:13 -0800

    adding seed each load

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,31 @@
-const { Client } = require('pg');
+const { Client, Pool } = require('pg');
+const fs = require('fs');
+const path = require('path');
+
 const env = process.env.NODE_ENV || "development";
 const config = require(__dirname + "/config.json")[env];
 
 
 // Checks if in production 
 if (config.use_env_variable) {
-    module.exports = new Client(process.env[config.use_env_variable]);
+    connectionObject = process.env[config.use_env_variable];
 } else {
-    module.exports = new Client(config);
+    connectionObject = config;
 }
+
+const schema = fs.readFileSync(path.resolve(__dirname + '/../db/schema.sql'), 'utf8');
+const seed = fs.readFileSync(path.resolve(__dirname + '/../db/seeds.sql'), 'utf8');
+
+const pool = new Pool(connectionObject);
+
+pool.query(schema, (error, response) => {
+    if (error) console.log(error);
+    pool.query(seed, (error, response) => {
+        if (error) console.log(error);
+        console.log("Seed complete");
+        pool.end();
+    });
+});
+
+
+module.exports = new Client(connectionObject);
