887b7937ab70632c63321371ef673601f156187d
Zarlengo <christopher@zarlengo.net>
2020-11-10 08:56:39 -0800

    Seed file

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(express.json());
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
+require(path.join(__dirname, '/db/seed'));
 
 const connection = require("./config/connection");
 
