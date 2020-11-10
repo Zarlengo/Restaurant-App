4a6f215fab8601c2c78087d408967cc6fcc658f7
Zarlengo <christopher@zarlengo.net>
2020-11-10 08:30:35 -0800

    heroku modification

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,6 @@
 const { Client } = require('pg');
+console.log(process.env.NODE_ENV);
+
 const connectionObject = require('./config.json')[process.env.NODE_ENV || "development"];
 
 console.log(connectionObject);
